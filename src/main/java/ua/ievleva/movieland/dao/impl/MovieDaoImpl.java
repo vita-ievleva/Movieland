7a1cc9320f68fcf31c829471113f0170103ad3a2
vi <vita.ievleva@gmail.com>
2016-12-06 14:44:04 +0200

    Added MovieDao interface

--- src/main/java/ua/ievleva/movieland/dao/impl/MovieDaoImpl.java
+++ src/main/java/ua/ievleva/movieland/dao/impl/MovieDaoImpl.java
@@ -5,19 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import ua.ievleva.movieland.dao.MovieDao;
 import ua.ievleva.movieland.dao.mapper.MovieMapper;
 import ua.ievleva.movieland.entity.Movie;
 
 import java.util.List;
 
 @Service
-public class MovieDaoImpl {
+public class MovieDaoImpl implements MovieDao {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Override
     public List<Movie> getAllMovies() {
         String sqlGetMovies = "select movie.id, name, year, rate, genre from movie inner join movie_genres on  movie.id=movie_genres.id_movie;";
 
