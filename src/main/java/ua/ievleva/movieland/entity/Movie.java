c28dd9dc9b9ce1747236f9cabddeb28668f1ee98
vi <vita.ievleva@gmail.com>
2016-12-08 13:18:54 +0200

    Modified Movie entity

--- src/main/java/ua/ievleva/movieland/entity/Movie.java
+++ src/main/java/ua/ievleva/movieland/entity/Movie.java
@@ -2,17 +2,47 @@ package ua.ievleva.movieland.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Movie {
+
     @JsonIgnore
     private String id;
 
     private String title;
     private String yearOfRelease;
     private String rating;
+    private String description;
+    private String country;
     private List<Genre> genres;
+    private List<Review> reviews;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 
     public String getId() {
         return id;
