f91feccf8faf27ae3f7c526d43ebddb0b45d1e60
vi <vita.ievleva@gmail.com>
2016-12-12 14:25:54 +0200

    Refactored after review

--- src/main/java/ua/ievleva/movieland/controller/MovieLandController.java
+++ src/main/java/ua/ievleva/movieland/controller/MovieLandController.java
@@ -1,30 +1,20 @@
 package ua.ievleva.movieland.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ua.ievleva.movieland.dao.MovieDao;
 
-import java.io.IOException;
 import java.util.Map;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class MovieLandController {
 
-    private final Logger logger = LoggerFactory.getLogger(MovieLandController.class);
-
-
     @Autowired
     private MovieDao movieDao;
-
-    @Autowired
-    private ObjectMapper jsonMapper;
-
 
     @GetMapping(value = "/v1/movies", produces = {"application/json", "application/xml"})
     public ResponseEntity<?> getAllMovies() {
@@ -32,26 +22,15 @@ public class MovieLandController {
         return new ResponseEntity<>(movieDao.getAllMovies(), HttpStatus.OK);
     }
 
-
     @GetMapping(value = "/v1/movie/{movieId}", produces = {"application/json", "application/xml"})
     public ResponseEntity<?> getMovieById(@PathVariable("movieId") String movieId) {
 
         return new ResponseEntity<>(movieDao.getMovieById(movieId), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/v1/search", produces = {"application/json", "application/xml"})
-    public ResponseEntity<?> searchMovies(@RequestBody String searchQuery) {
+    @RequestMapping(value = "/v1/search",method =  GET, consumes = "application/json", produces = {"application/json", "application/xml"})
+    public ResponseEntity<?> searchMovies(@RequestBody Map<String, String> searchQuery) {
 
-        Map<String, String> searchParameters = null;
-
-        try {
-             searchParameters = jsonMapper.readValue(searchQuery, Map.class);
-        } catch (IOException e) {
-            logger.error("Search query is invalid", e);
-        }
-
-        return new ResponseEntity<>(movieDao.searchMovies(searchParameters), HttpStatus.FOUND);
-
+        return new ResponseEntity<>(movieDao.searchMovies(searchQuery), HttpStatus.OK);
     }
-
 }
--- src/main/java/ua/ievleva/movieland/dao/impl/MovieDaoImpl.java
+++ src/main/java/ua/ievleva/movieland/dao/impl/MovieDaoImpl.java
@@ -1,13 +1,14 @@
 package ua.ievleva.movieland.dao.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 import ua.ievleva.movieland.dao.MovieDao;
-import ua.ievleva.movieland.dao.mapper.*;
+import ua.ievleva.movieland.dao.mapper.GenresByIdMapper;
+import ua.ievleva.movieland.dao.mapper.MovieByIdMapper;
+import ua.ievleva.movieland.dao.mapper.MovieMapper;
+import ua.ievleva.movieland.dao.mapper.ReviewByIdMapper;
 import ua.ievleva.movieland.entity.Genre;
 import ua.ievleva.movieland.entity.Movie;
 import ua.ievleva.movieland.entity.Review;
@@ -16,74 +17,65 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import static ua.ievleva.movieland.dao.util.QueryBuilderUtil.buildConditionQuery;
+
 @Service
 public class MovieDaoImpl implements MovieDao {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    private static final MovieMapper MOVIE_MAPPER = new MovieMapper();
+    private static final MovieByIdMapper MOVIE_BY_ID_MAPPER = new MovieByIdMapper();
+    private static final ReviewByIdMapper REVIEW_BY_ID_MAPPER = new ReviewByIdMapper();
+    private static final GenresByIdMapper GENRES_BY_ID_MAPPER = new GenresByIdMapper();
 
     @Autowired
-    private Properties sqls;
+    private Properties sqlsQueries;
 
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public List<Movie> getAllMovies() {
-        String sql = sqls.getProperty("movies.select.allMovies");
+        String sql = sqlsQueries.getProperty("movies.select.allMovies");
 
-        return namedParameterJdbcTemplate.query(sql, new MovieMapper());
-
+        return namedParameterJdbcTemplate.query(sql, MOVIE_MAPPER);
     }
 
     @Override
     public Movie getMovieById(String movieId) {
-        String sql = sqls.getProperty("movie.select.byId");
+        String sql = sqlsQueries.getProperty("movie.select.byId");
 
         Movie movie = namedParameterJdbcTemplate.queryForObject(sql,
                 new MapSqlParameterSource("id", movieId),
-                new MovieByIdMapper());
+                MOVIE_BY_ID_MAPPER);
 
-        movie.setReviews(getReviewsOfMovie(movieId));
-        movie.setGenres(getGenresOfMovie(movieId));
+        movie.setReviews(getReviewsByMovieId(movieId));
+        movie.setGenres(getGenresByMovieId(movieId));
 
         return movie;
     }
 
-
     @Override
     public List<Movie> searchMovies(Map<String, String> searchParameters) {
-        String sql = sqls.getProperty("movies.select.bySearchParameters");
+        String sql = sqlsQueries.getProperty("movies.select.bySearchParameters");
         sql = sql.replace("[conditions]", buildConditionQuery(searchParameters));
 
-        return namedParameterJdbcTemplate.query(sql, new MovieMapper());
+        return namedParameterJdbcTemplate.query(sql, MOVIE_MAPPER);
     }
 
-    private String buildConditionQuery(Map<String, String> searchParameters) {
-
-        StringBuilder sb = new StringBuilder();
-
-        searchParameters.forEach((k, v) -> {
-            sb.append(k).append("=\"").append(v).append("\" AND ");
-
-        });
-
-        return sb.delete(sb.length() - 4, sb.length() - 1).toString();
-    }
-
-    private List<Review> getReviewsOfMovie(String movieId) {
-        String sql = sqls.getProperty("review.select.byMovieId");
+    private List<Review> getReviewsByMovieId(String movieId) {
+        String sql = sqlsQueries.getProperty("review.select.byMovieId");
 
         return namedParameterJdbcTemplate.query(sql,
                 new MapSqlParameterSource("id", movieId),
-                new ReviewByIdMapper());
+                REVIEW_BY_ID_MAPPER);
     }
 
-    private List<Genre> getGenresOfMovie(String movieId) {
-        String sql = sqls.getProperty("genre.select.byMovieId");
+    private List<Genre> getGenresByMovieId(String movieId) {
+        String sql = sqlsQueries.getProperty("genre.select.byMovieId");
 
         return namedParameterJdbcTemplate.query(sql,
                 new MapSqlParameterSource("id", movieId),
-                new GenresByIdMapper());
+                GENRES_BY_ID_MAPPER);
     }
 
 }
