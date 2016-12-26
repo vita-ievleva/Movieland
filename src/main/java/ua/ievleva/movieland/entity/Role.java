69d3f9aa819f267d1897d04bbf9e76706a61ba13
vi <vita.ievleva@gmail.com>
2016-12-26 14:14:33 +0200

    Added new entities

---
+++ src/main/java/ua/ievleva/movieland/entity/Role.java
@@ -0,0 +1,6 @@
+package ua.ievleva.movieland.entity;
+
+
+public enum  Role {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/ua/ievleva/movieland/entity/User.java
@@ -0,0 +1,73 @@
+package ua.ievleva.movieland.entity;
+
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String email;
+    private String userName;
+    private String password;
+    private String enabled;
+    private List<Role> role;
+
+    public List<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(List<Role> role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(String enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", enabled='" + enabled + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
