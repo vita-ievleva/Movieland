d539b07cb326a03ed2308005e5fbca2b2d252062
vi <vita.ievleva@gmail.com>
2016-12-26 14:30:54 +0200

    Added UserDao

---
+++ src/main/java/ua/ievleva/movieland/dao/impl/UserDaoImpl.java
@@ -0,0 +1,45 @@
+package ua.ievleva.movieland.dao.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+import ua.ievleva.movieland.exception.InvalidCredentialsException;
+import ua.ievleva.movieland.dao.UserDao;
+import ua.ievleva.movieland.dao.mapper.UserMapper;
+import ua.ievleva.movieland.entity.User;
+import ua.ievleva.movieland.security.token.TokenUtils;
+
+import java.util.Map;
+import java.util.Properties;
+
+@Component
+public class UserDaoImpl implements UserDao {
+
+    private static UserMapper USER_MAPPER = new UserMapper();
+
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Autowired
+    private Properties sqlQueries;
+
+    @Override
+    public User findUserByCredentials(Map<String, String> credentials) throws InvalidCredentialsException {
+        String sql = sqlQueries.getProperty("users.byUserName");
+
+        User user = namedParameterJdbcTemplate.queryForObject(sql,
+                new MapSqlParameterSource("username", credentials.get("username")),
+                USER_MAPPER);
+
+        if(!tokenUtils.isCorrectPassword(credentials.get("password"), user.getPassword())) {
+            throw new InvalidCredentialsException("Invalid password.");
+        }
+
+        return user;
+    }
+
+}
---
+++ src/main/java/ua/ievleva/movieland/exception/InvalidCredentialsException.java
@@ -0,0 +1,10 @@
+package ua.ievleva.movieland.exception;
+
+
+public class InvalidCredentialsException extends Exception {
+
+    public InvalidCredentialsException(String s) {
+        super(s);
+    }
+}
+
