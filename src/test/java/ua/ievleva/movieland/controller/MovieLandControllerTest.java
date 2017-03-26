a40ea7a4e0371841d578d8bf6e64f9d997fc189c
vi <vita.ievleva@gmail.com>
2017-03-26 16:56:21 +0300

    Added Basic Integration test for MovieController

---
+++ src/test/java/ua/ievleva/movieland/controller/MovieLandControllerTest.java
@@ -0,0 +1,106 @@
+package ua.ievleva.movieland.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/applicationContext.xml"})
+@WebAppConfiguration
+public class MovieLandControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testServletContextProvidesMovielandController() {
+        ServletContext servletContext = wac.getServletContext();
+
+        Assert.assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(wac.getBean("movieLandController"));
+
+
+    }
+
+    @Test
+    public void getAllMovies() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/v1/movies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].price").value(123.45))
+                .andExpect(jsonPath("$.[0].yearOfRelease").value("1994"))
+                .andExpect(jsonPath("$.[24].price").value(100.0))
+                .andExpect(jsonPath("$.[24].yearOfRelease").value("1976"));
+    }
+
+    @Test
+    public void getAllMoviesWithSortingPrice() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/v1/movies?price=desc"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].price", greaterThan(200.00)));
+    }
+
+    @Test
+    public void getAllMoviesWithSortingRate() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/v1/movies?rate=desc"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].rating", greaterThan(8.00)));
+    }
+
+    @Test
+    public void getAllMoviesWithSortingRateAndPrice() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/v1/movies?price=desc&rate=asc"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].rating", Matchers.is(8.6)))
+                .andExpect(jsonPath("$.[0].price", Matchers.is(200.6)));
+    }
+
+    @Test
+    public void getMovieById() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/v1/movie/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(134.67))
+                .andExpect(jsonPath("$.title").value("Зеленая миля/The Green Mile"));
+    }
+
+    @Test
+    public void searchMovies() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put("genre", "комедия");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/v1/search")
+                .content("{\"genre\": \"комедия\", \"country\": \"США\"}").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].price").value(170.00))
+                .andExpect(jsonPath("$.[0].title").value("Джанго освобожденный/Django Unchained"));
+    }
+
+
+}
