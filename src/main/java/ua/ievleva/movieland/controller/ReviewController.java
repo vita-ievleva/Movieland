0574b7ea0e64ffde805ed96b39359099c2a3214d
Viktoriya Iyevleva <vita.ievleva@gmai.com>
2017-04-05 14:39:21 +0300

    Refactored ReviewController

--- src/main/java/ua/ievleva/movieland/controller/ReviewController.java
+++ src/main/java/ua/ievleva/movieland/controller/ReviewController.java
@@ -3,17 +3,15 @@ package ua.ievleva.movieland.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ua.ievleva.movieland.security.token.TokenUtils;
 import ua.ievleva.movieland.service.ReviewService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
-
+@RequestMapping("/v1")
 @RestController
 public class ReviewController {
 
@@ -23,15 +21,18 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
-    @PostMapping(value = "/v1/review")
+    @PostMapping(value = "/review", consumes = "application/json", produces = {"application/json", "application/xml"})
     public ResponseEntity<?> addReview(@RequestBody Map<String, String> review, HttpServletRequest request) {
         String token = request.getHeader("Authorization");
         review.put("username", tokenUtils.parseToken(token).getSubject());
 
-        return new ResponseEntity<>(reviewService.addReview(review),HttpStatus.OK);
+        Map<String, Boolean> result = new HashMap<>();
+        result.put("result", reviewService.addReview(review));
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @DeleteMapping(value = "/v1/review")
+    @DeleteMapping(value = "/review")
     public ResponseEntity<?> deleteReview(@RequestBody Map<String, String> review, HttpServletRequest request) {
         String token = request.getHeader("Authorization");
         review.put("username", tokenUtils.parseToken(token).getSubject());
