780dcee2f4b7ef73d34a5c22f850fae710690713
Viktoriya Iyevleva <vita.ievleva@gmai.com>
2017-04-10 15:08:05 +0300

    Removed profile

--- src/test/java/ua/ievleva/movieland/controller/MovieLandIntegrationTest.java
+++ src/test/java/ua/ievleva/movieland/controller/MovieLandIntegrationTest.java
@@ -1,7 +1,6 @@
 package ua.ievleva.movieland.controller;
 
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -10,7 +9,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/applicationContext.xml"})
 @WebAppConfiguration
-@ActiveProfiles("test")
 public abstract class MovieLandIntegrationTest {
 
 }
